338705ca6e8432718e23910f444d30d33c7fc369
MaxWill <maxwill.market@gmail.com>
2019-08-19 00:44:25 +0300

    3 задание без чекбоксов

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,7 @@ class IndexController < ApplicationController
     @projects=Project.all
     @projects_titles=Project.all.map{|u| [ u.title, u.id ] }
   end
+
   def create
     @todo=Todo.new(todo_params)
     @todo.save
@@ -10,8 +11,18 @@ class IndexController < ApplicationController
     # render plain: params[:todo_create].inspect
   end
 
+  def update
+    @todos = Todo.find_by(params.require(:todos).permit(:id))
+    @todoo = @todos.update(bool)
+    # redirect_to action: "index"
+  end
+
   private def todo_params
     params.require(:todo_create).permit(:text, :project_id)
+  end
+
+  def bool
+    params.require(:todos).permit(:isCompleted)
   end
 
 end
